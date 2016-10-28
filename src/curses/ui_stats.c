eeafde121e6e6df3e26561b20dc2123ebf874526
Kristian Høgh <kfh@uni-tel.dk>
2016-10-28 09:06:14 +0200

    Include busy and diverted calls in Stats Information

--- src/curses/ui_stats.c
+++ src/curses/ui_stats.c
@@ -33,6 +33,8 @@
  * |  Calls: 10                     CANCELLED:  2 (12.2%)    |
  * |  Messages: 200                 IN CALL:    10 (60.5%)   |
  * |                                REJECTED:   0 (0.0%)     |
+ * |                                BUSY:       0 (0.0%)     |
+ * |                                DIVERTED:   0 (0.0%)     |
  * |                                CALL SETUP: 0 (0.0%)     |
  * +---------------------------------------------------------+
  * |  INVITE:    10 (0.5%)          1XX: 123 (1.5%)          |
@@ -44,7 +46,6 @@
  * |  PUBLISH:   0 (0.0%)           7XX: 0 (0.0%)            |
  * |  MESSAGE:   0 (0.0%)           8XX: 0 (0.0%)            |
  * |  INFO:      0 (0.0%)                                    |
- * |  ACK:       300 (7.2%)                                  |
  * |  BYE:       10 (0.5%)                                   |
  * |  CANCEL:    0 (0.0%)                                    |
  * +---------------------------------------------------------+
@@ -79,7 +80,7 @@ stats_create(ui_t *ui)
 
     // Counters!
     struct {
-        int dtotal, dcalls, completed, cancelled, incall, rejected, setup;
+        int dtotal, dcalls, completed, cancelled, incall, rejected, setup, busy, diverted;
         int mtotal, invite, regist, subscribe, update, notify, options;
         int publish, message, info, ack, bye, cancel;
         int r100, r200, r300, r400, r500, r600, r700, r800;
@@ -87,15 +88,15 @@ stats_create(ui_t *ui)
     memset(&stats, 0, sizeof(stats));
 
     // Calculate window dimensions
-    ui_panel_create(ui, 23, 60);
+    ui_panel_create(ui, 25, 60);
 
     // Set the window title and boxes
     mvwprintw(ui->win, 1, ui->width / 2 - 9, "Stats Information");
     wattron(ui->win, COLOR_PAIR(CP_BLUE_ON_DEF));
     title_foot_box(ui->panel);
-    mvwhline(ui->win, 8, 1, ACS_HLINE, ui->width - 1);
-    mvwaddch(ui->win, 8, 0, ACS_LTEE);
-    mvwaddch(ui->win, 8, ui->width - 1, ACS_RTEE);
+    mvwhline(ui->win, 10, 1, ACS_HLINE, ui->width - 1);
+    mvwaddch(ui->win, 10, 0, ACS_LTEE);
+    mvwaddch(ui->win, 10, ui->width - 1, ACS_RTEE);
     mvwprintw(ui->win, ui->height - 2, ui->width / 2 - 9, "Press ESC to leave");
     wattroff(ui->win, COLOR_PAIR(CP_BLUE_ON_DEF));
 
@@ -120,6 +121,8 @@ stats_create(ui_t *ui)
                 case SIP_CALLSTATE_INCALL: stats.incall++; break;
                 case SIP_CALLSTATE_CANCELLED: stats.cancelled++; break;
                 case SIP_CALLSTATE_REJECTED: stats.rejected++; break;
+                case SIP_CALLSTATE_BUSY: stats.busy++; break;
+                case SIP_CALLSTATE_DIVERTED: stats.diverted++; break;
                 case SIP_CALLSTATE_COMPLETED: stats.completed++; break;
             }
         }
@@ -169,27 +172,29 @@ stats_create(ui_t *ui)
         mvwprintw(ui->win, 4,  33, "CANCELLED:  %d (%.1f\%)", stats.cancelled, (float) stats.cancelled * 100 / stats.dcalls);
         mvwprintw(ui->win, 5,  33, "IN CALL:    %d (%.1f\%)", stats.incall,    (float) stats.incall * 100 / stats.dcalls);
         mvwprintw(ui->win, 6,  33, "REJECTED:   %d (%.1f\%)", stats.rejected,  (float) stats.rejected * 100 / stats.dcalls);
-        mvwprintw(ui->win, 7,  33, "CALL SETUP: %d (%.1f\%)", stats.setup,     (float) stats.setup * 100 / stats.dcalls);
+        mvwprintw(ui->win, 7,  33, "BUSY:       %d (%.1f\%)", stats.busy,      (float) stats.busy * 100 / stats.dcalls);
+        mvwprintw(ui->win, 8,  33, "DIVERTED:   %d (%.1f\%)", stats.diverted,  (float) stats.diverted * 100 / stats.dcalls);
+        mvwprintw(ui->win, 9,  33, "CALL SETUP: %d (%.1f\%)", stats.setup,     (float) stats.setup * 100 / stats.dcalls);
     }
 
-    mvwprintw(ui->win, 9,  3, "INVITE:    %d (%.1f\%)", stats.invite,    (float) stats.invite * 100 / stats.mtotal);
-    mvwprintw(ui->win, 10, 3, "REGISTER:  %d (%.1f\%)", stats.regist,    (float) stats.regist * 100 / stats.mtotal);
-    mvwprintw(ui->win, 11, 3, "SUBSCRIBE: %d (%.1f\%)", stats.subscribe, (float) stats.subscribe * 100 / stats.mtotal);
-    mvwprintw(ui->win, 12, 3, "UPDATE:    %d (%.1f\%)", stats.update,    (float) stats.update * 100 / stats.mtotal);
-    mvwprintw(ui->win, 13, 3, "NOTIFY:    %d (%.1f\%)", stats.notify,    (float) stats.notify * 100 / stats.mtotal);
-    mvwprintw(ui->win, 14, 3, "OPTIONS:   %d (%.1f\%)", stats.options,   (float) stats.options * 100 / stats.mtotal);
-    mvwprintw(ui->win, 15, 3, "PUBLISH:   %d (%.1f\%)", stats.publish,   (float) stats.publish * 100 / stats.mtotal);
-    mvwprintw(ui->win, 16, 3, "MESSAGE:   %d (%.1f\%)", stats.message,   (float) stats.message * 100 / stats.mtotal);
-    mvwprintw(ui->win, 17, 3, "INFO:      %d (%.1f\%)", stats.info,      (float) stats.info * 100 / stats.mtotal);
-    mvwprintw(ui->win, 18, 3, "BYE:       %d (%.1f\%)", stats.bye,       (float) stats.bye * 100 / stats.mtotal);
-    mvwprintw(ui->win, 19, 3, "CANCEL:    %d (%.1f\%)", stats.cancel,    (float) stats.cancel * 100 / stats.mtotal);
+    mvwprintw(ui->win, 11, 3, "INVITE:    %d (%.1f\%)", stats.invite,    (float) stats.invite * 100 / stats.mtotal);
+    mvwprintw(ui->win, 12, 3, "REGISTER:  %d (%.1f\%)", stats.regist,    (float) stats.regist * 100 / stats.mtotal);
+    mvwprintw(ui->win, 13, 3, "SUBSCRIBE: %d (%.1f\%)", stats.subscribe, (float) stats.subscribe * 100 / stats.mtotal);
+    mvwprintw(ui->win, 14, 3, "UPDATE:    %d (%.1f\%)", stats.update,    (float) stats.update * 100 / stats.mtotal);
+    mvwprintw(ui->win, 15, 3, "NOTIFY:    %d (%.1f\%)", stats.notify,    (float) stats.notify * 100 / stats.mtotal);
+    mvwprintw(ui->win, 16, 3, "OPTIONS:   %d (%.1f\%)", stats.options,   (float) stats.options * 100 / stats.mtotal);
+    mvwprintw(ui->win, 17, 3, "PUBLISH:   %d (%.1f\%)", stats.publish,   (float) stats.publish * 100 / stats.mtotal);
+    mvwprintw(ui->win, 18, 3, "MESSAGE:   %d (%.1f\%)", stats.message,   (float) stats.message * 100 / stats.mtotal);
+    mvwprintw(ui->win, 19, 3, "INFO:      %d (%.1f\%)", stats.info,      (float) stats.info * 100 / stats.mtotal);
+    mvwprintw(ui->win, 20, 3, "BYE:       %d (%.1f\%)", stats.bye,       (float) stats.bye * 100 / stats.mtotal);
+    mvwprintw(ui->win, 21, 3, "CANCEL:    %d (%.1f\%)", stats.cancel,    (float) stats.cancel * 100 / stats.mtotal);
 
-    mvwprintw(ui->win, 9,  33, "1XX: %d (%.1f\%)", stats.r100, (float) stats.r100 * 100 / stats.mtotal);
-    mvwprintw(ui->win, 10, 33, "2XX: %d (%.1f\%)", stats.r200, (float) stats.r200 * 100 / stats.mtotal);
-    mvwprintw(ui->win, 11, 33, "3XX: %d (%.1f\%)", stats.r300, (float) stats.r300 * 100 / stats.mtotal);
-    mvwprintw(ui->win, 12, 33, "4XX: %d (%.1f\%)", stats.r400, (float) stats.r400 * 100 / stats.mtotal);
-    mvwprintw(ui->win, 13, 33, "5XX: %d (%.1f\%)", stats.r500, (float) stats.r500 * 100 / stats.mtotal);
-    mvwprintw(ui->win, 14, 33, "6XX: %d (%.1f\%)", stats.r600, (float) stats.r600 * 100 / stats.mtotal);
-    mvwprintw(ui->win, 15, 33, "7XX: %d (%.1f\%)", stats.r700, (float) stats.r700 * 100 / stats.mtotal);
-    mvwprintw(ui->win, 16, 33, "8XX: %d (%.1f\%)", stats.r800, (float) stats.r800 * 100 / stats.mtotal);
+    mvwprintw(ui->win, 11, 33, "1XX: %d (%.1f\%)", stats.r100, (float) stats.r100 * 100 / stats.mtotal);
+    mvwprintw(ui->win, 12, 33, "2XX: %d (%.1f\%)", stats.r200, (float) stats.r200 * 100 / stats.mtotal);
+    mvwprintw(ui->win, 13, 33, "3XX: %d (%.1f\%)", stats.r300, (float) stats.r300 * 100 / stats.mtotal);
+    mvwprintw(ui->win, 14, 33, "4XX: %d (%.1f\%)", stats.r400, (float) stats.r400 * 100 / stats.mtotal);
+    mvwprintw(ui->win, 15, 33, "5XX: %d (%.1f\%)", stats.r500, (float) stats.r500 * 100 / stats.mtotal);
+    mvwprintw(ui->win, 16, 33, "6XX: %d (%.1f\%)", stats.r600, (float) stats.r600 * 100 / stats.mtotal);
+    mvwprintw(ui->win, 17, 33, "7XX: %d (%.1f\%)", stats.r700, (float) stats.r700 * 100 / stats.mtotal);
+    mvwprintw(ui->win, 18, 33, "8XX: %d (%.1f\%)", stats.r800, (float) stats.r800 * 100 / stats.mtotal);
 }
